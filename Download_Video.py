2398fbfc8223a63b0d67ddcd8109773e360bb7ab
ArthurWalker <phuc.phamhuu2@dcu.ie>
2019-04-26 15:53:16 +0100

    Get a full list of video IDs

--- Download_Video.py
+++ Download_Video.py
@@ -6,6 +6,12 @@ import youtube_dl
 # Settings for downloading:
 #        https://github.com/ytdl-org/youtube-dl/blob/611c1dd96efc36a788475e14cc4de64d554d28a0/youtube_dl/YoutubeDL.py#L248
 
+video_ID_dict = {}
+def retrieve_video_ID_list(page):
+    # Get a dictionary of all Video IDs in 1 response
+    for video in page:
+        video_ID_dict[video['snippet']['resourceId']['videoId']]={}
+
 def prepare_API_request(api_key,playlistID):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production
@@ -18,32 +24,47 @@ def prepare_API_request(api_key,playlistID):
 
     # Execute the request
     youtube = googleapiclient.discovery.build(api_service_name,api_version,developerKey=dev_api_key)
-    request = youtube.playlistItems().list(
-        part = 'snippet',
-        maxResults=20,
-        playlistId=playlistID,
-        pageToken=''
-    )
-    response = request.execute()
-    return response
 
-def display_Info_response(response):
+    # Request playlist and store the video IDs
+    flag=True
+    pageToken = ''
+    while flag:
+        request_playlist = youtube.playlistItems().list(
+            part = 'snippet',
+            maxResults=50,
+            playlistId=playlistID,
+            pageToken=pageToken,
+        )
+        response_playlist = request_playlist.execute()
+        page = response_playlist['items']
+        retrieve_video_ID_list(page)
+        if 'nextPageToken' not in response_playlist.keys():
+            flag=False
+            break
+        pageToken = response_playlist['nextPageToken']
+
+    # Request video duration
+    # request_duration = youtube.videos().list(
+    #     part='contentDetails',
+    #     id = 'iNSGvZRgjcQ'
+    # )
+    # response_duration = request_duration.execute()
+    response_duration = ''
+    return (response_playlist,response_duration)
+
+def display_Info_response(response_playlist,response_duration):
     # Display response
     # General Information
-    print('General information is {}. Each page contains {}'.format(response['pageInfo'], list(response.keys())))
-    print('Next page token:', response['nextPageToken'])
-
-def display_Info_eachPage(page):
-    # Information for each page
-    for each_video in page:
-        #print (each_page['snippet']['title'])
-        print (list(each_video.keys()))
+    print('General information is {}. Each page contains {}'.format(response_playlist['pageInfo'], list(response_playlist.keys())))
+    print('Next page token:', response_playlist['nextPageToken'])
+    #print('Duration:',response_duration['items'][0]['contentDetails']['duration'])
 
 def display_Info_one_video(page):
     # Information for one video
     some_vid = page[1]
     some_vid_info = some_vid['snippet']
-    print ('The first video ID is {}.\nIt contains {}'.format(some_vid['id'],some_vid_info.keys()))
+    print ('Keys of some vid:',some_vid.keys())
+    print ('The first response ID is {}.\nIt contains {}'.format(some_vid['id'],some_vid_info.keys()))
     print ('Playlist ID:',some_vid_info['playlistId'])
     print ('Channel Titile:',some_vid_info['channelTitle'])
     print ('Channel ID:',some_vid_info['channelId'])
@@ -51,12 +72,9 @@ def display_Info_one_video(page):
     print ('Position:',some_vid_info['position'])
     print ('Video ID:',some_vid_info['resourceId']['videoId'])
 
-def display_video_ID_list(page):
-    # Get a list of all Video IDs in 1 response
-    lst_video_id = []
-    for video in page:
-        lst_video_id.append(video['snippet']['resourceId']['videoId'])
-    return lst_video_id
+def display_Information(page):
+    # Information
+    display_Info_one_video(page)
 
 def create_directory():
     path = os.path.join('C:/Users/pphuc/Desktop/Docs/Current Using Docs/')
@@ -106,7 +124,7 @@ def download_Youtube_playlist(default_link_playlist):
         'noplaylist':False,
         'playlistend':3,
         # Remove the download discription
-        'quiet':True,
+        'quiet':False,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -120,35 +138,35 @@ def download_Youtube_playlist(default_link_playlist):
     except Exception as err:
         print ('!!!!!!!Problem downloading with',default_link_playlist,'as follow',err)
 
+def download_Functions(page,playlist_ID):
+    # Download a single audio
+    # To access to each video then just need its ID. The link to access is https://www.youtube.com/watch?v=<VIDEO ID>
+    default_link = 'https://www.youtube.com/watch?v='
+    sample_video_info = page[2]['snippet']
+    download_one_Youtube_audio(sample_video_info,default_link)
+    download_one_Youtube_video(sample_video_info,default_link)
+
+    # Download Playlist
+    # To access to each playlist then just need its ID. The link to access is https://www.youtube.com/playlist?list=
+    default_link_playlist='https://www.youtube.com/playlist?list='+playlist_ID
+    download_Youtube_playlist(default_link_playlist)
 
 def main():
-
+    # Default Variables
     playlist_ID = 'PL0an7prpX1ERTY4ohSEP2ZzQVkViY91ql'
     API_key = '**REMOVED**'
-    video_id= ''
-    response = prepare_API_request(API_key,playlist_ID)
 
-    display_Info_response(response)
+    # Prepare API requests
+    response_playlist,response_duration = prepare_API_request(API_key,playlist_ID)
+    #display_Info_response(response_playlist,response_duration)
 
-    page = response['items']
+    page = response_playlist['items']
 
-    # display_Info_eachPage(page)
-
-    # display_Info_one_video(page)
-
-    # display_video_ID_list(page)
-
-    create_directory()
-
-    # To access to each video then just need its ID. The link to access is https://www.youtube.com/watch?v=<VIDEO ID>
-    # default_link = 'https://www.youtube.com/watch?v='
-    # sample_video_info = page[2]['snippet']
-    # download_one_Youtube_video(sample_video_info,default_link)
-    # download_one_Youtube_audio(sample_video_info,default_link)
-
-    # To access to each playlist then just need its ID. The link to access is https://www.youtube.com/playlist?list=
-    # default_link_playlist='https://www.youtube.com/playlist?list='+playlist_ID
-    # download_Youtube_playlist(default_link_playlist)
+    display_Information(page)
+    #
+    # create_directory()
+    #
+    # download_Functions(page,playlist_ID)
 
 if __name__ == '__main__':
     main()
